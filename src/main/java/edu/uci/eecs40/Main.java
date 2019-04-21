64480a71240165cb8e368c1c6adc05e07937fdb9
clyton <clyton.dantis@gmail.com>
2019-04-20 20:30:42 -0700

    EECS 40 - Assignment 2 pom updated java version

--- src/main/java/edu/uci/eecs40/Main.java
+++ src/main/java/edu/uci/eecs40/Main.java
@@ -3,6 +3,6 @@ package edu.uci.eecs40;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hi UCI EECS 40 " + args[0]);
+        System.out.println("Hello UCI EECS 40 " + args[0]);
     }
 }
