daea446d8e6c29ffe96a9f48991b4fde005d3d27
Nathaniel Aponte <aponten@uci.edu>
2019-04-25 22:38:23 -0700

    Added instruction class

---
+++ src/com/company/Instructions.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public class Instructions {
+/*Each class still needs to have its parameters determined but should be the same
+for all classes */
+
+    public class DAT{
+    /*only keeps a value in the b field but if excicuted player loses*/
+
+    }
+
+    public class MOV{
+    /*Takes instruction from address determined by A and B field and copies it to
+    new address also depending on A and B field*/
+
+    }
+
+    public class ADD{
+
+    }
+
+    public class SUB{
+
+    }
+
+    public class JMP{
+
+    }
+
+    public class JMZ{
+
+    }
+
+    public class DJN{
+
+    }
+
+    public class CMP{
+
+    }
+
+    public class SPL{
+
+    }
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,8 @@ import java.io.FileReader;
 import java.io.IOException;
 */
 
+	/*Maybe consider doing another array or two (a and b field) to keep track of values
+	and to be make it easier to manipulate said values */
         public static void main(String[] args) {
             //defining objects
             OpenFile objP1 = new OpenFile();
